f57b063a9573882a524f0c6bf8c4a2f50dc97c29
janghy193 <54100092+janghy193@users.noreply.github.com>
2021-06-10 19:34:07 +0900

    err accrued

--- src/main/java/com/hydemo/chat/controller/ChatController.java
+++ src/main/java/com/hydemo/chat/controller/ChatController.java
@@ -1,20 +1,14 @@
 package com.hydemo.chat.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
-	@Autowired
-	ResourceLoader resourceLoader;
 	
 	@GetMapping("/")
-	public String mainPage() {
-		// System.out.println("loaded");
+	public String mainPageDefault() {
 		return "main";
 	}
-	
 	
 }
